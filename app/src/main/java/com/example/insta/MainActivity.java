b755764b429fa61a39d2e97a21095bb617b55292
kevinjacob004 <kevinjacobmathew.004@gmail.com>
2024-12-18 22:05:17 +0530

    home

--- app/src/main/java/com/example/insta/MainActivity.java
+++ app/src/main/java/com/example/insta/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.insta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +15,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText e1,e2;
+    Button b1,b2;
+    String getUname,getPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        e1=(EditText) findViewById(R.id.uname);
+        e2=(EditText) findViewById(R.id.pass);
+        b1=(Button) findViewById(R.id.but1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getUname=e1.getText().toString();
+                getPass=e2.getText().toString();
+
+                if(getUname.equals("admin")&&getPass.equals("12345")){
+                    Intent ob=new Intent(getApplicationContext(),LogActivity.class);
+                    startActivity(ob);
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
     }
 }
